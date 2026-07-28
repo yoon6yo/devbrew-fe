170c1e7a8f8552ab7980a6609f9d69dbfe0ddb0a
yoon6yo <yukyum06@gmail.com>
2026-07-28 17:10:20 +0900

    fix: add missing useEffect import in DashboardPage

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams, Link } from 'react-router-dom'
 import { useQueryClient } from '@tanstack/react-query'
 import { useIdeas } from '@/hooks/useIdeas'
